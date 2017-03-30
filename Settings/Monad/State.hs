08d4b66af7f50635660506902c17bac0aa246ef6
yifan zhao <yifan zhao>
2017-03-30 15:33:53 +0800

    Diminished data EnvS (temp.ly)

--- Settings/Monad/State.hs
+++ Settings/Monad/State.hs
@@ -6,52 +6,15 @@
 
 module Settings.Monad.State (
     EnvS,
-    MonadEnvState,
-    tryGetEnvS,
     getEnvS,
     module Control.Monad.State
 ) where
 
 import           Control.Monad.State
-import           Control.Monad.Trans.Except
-import           Data.Aeson
-import           Data.Aeson.Types
-import           Data.Bifunctor
-import qualified Data.ByteString.Lazy.Char8          as L
-import           GHC.Generics
 
-import           Settings.Exception.GeneralException
-import           Settings.Monad.Exception
-
-data EnvS = EnvS {
-    getUserToken   :: String,
-    getDefaultPath :: String
-} deriving (Show, Generic)
-
-instance FromJSON EnvS where
-    parseJSON (Object obj) = EnvS
-                         <$> obj .: "token"
-                         <*> obj .: "download_path"
-    parseJSON invalid = typeMismatch "EnvS" invalid
-
-instance ToJSON EnvS where
-    toJSON envS =
-        object [ "token" .= getUserToken envS
-               , "download_path" .= getDefaultPath envS
-               ]
-    toEncoding = genericToEncoding defaultOptions
+data EnvS = EnvS deriving (Show)
 
 getEnvS :: IO EnvS
-getEnvS = return $ EnvS "" ""
-
-tryGetEnvS :: FilePath -> IO (Maybe EnvS)
-tryGetEnvS path = do
-    maybeEnvS <- runExceptT $ tryReadEnvS path
-    return $ either (const Nothing) Just maybeEnvS
-
-tryReadEnvS :: MonadIOE SomeException m => FilePath -> m EnvS
-tryReadEnvS path = do
-    file <- catchIOE $ L.readFile path
-    eitherToE $ first fromString $ eitherDecode file
+getEnvS = return EnvS
 
 type MonadEnvState m = MonadState EnvS m
